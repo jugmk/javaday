d9c693502a952b363fde539756fd4ffc23506dd9
Mite Mitreski <mitemitreski@gmail.com>
2013-12-12 23:49:32 +0100

    added logos and page cleanup + added bios

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,39 +1,22 @@
-//TODO:massive cleanup needed
 $(function () {
-  function bio(photo, text) {
-    return '<div class="media"><img class="media-object pull-left photo img-rounded" src="' + photo + '" /><div class="media-body">' + text + '</div></div>';
-  }
 
-  function rowClick() {
-    var tds = $(this).children();
-    var time = tds.get(0).innerHTML;
+$("a[href^='#']").on('click', function(e) {
 
-    if (!$(tds.get(1)).children().get(0)) return;
+   // prevent default anchor click behavior
+   e.preventDefault();
 
-    var title1 = $(tds.get(1)).children().get(0).innerHTML;
-    var title2 = $(tds.get(1)).children().get(1).innerHTML;
+   var hash = this.hash;
 
-    var speaker1 = $(tds.get(2)).children().get(0).innerHTML;
-    var speaker2 = $(tds.get(2)).children().get(1).innerHTML;
-    var speakers = speaker2.split("---");
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top
+     }, 300, function(){
 
-    var photos = $(tds.get(2)).children().get(1).getAttribute("data-photos");
-    photos = photos != null ? photos.split(",") : [''];
-    var two = photos.length == 2 || speakers.length == 2;
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
 
-    $("#modal-title").html(title1);
-    $("#modal-speaker").html(speaker1);
+});
 
-    $("#talk-details").html(title2);
-
-    var sp1 = bio(photos[0], speakers[0]);
-    var sp2 = two ? bio(photos[1], speakers[1]) : '';
-
-
-    $("#speaker-details").html(sp1 + sp2);
-
-    $("#info").modal('show');
-  }
-
-  $('tr').click(rowClick);
 });
